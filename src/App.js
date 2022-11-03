2b65d92d8fd8566e5ab617110e73346fead681a5
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2022-11-03 15:11:05 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Album from './Components/Album';
+import Favorites from './Components/Favorites';
+import Login from './Components/Login';
+import Search from './Components/Search';
+import Profile from './Components/Profile';
+import ProfileEdit from './Components/ProfileEdit';
+import NotFound from './Components/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes.</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <Route path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route component={ NotFound } />
+      </>
+    );
   }
 }
 
