2ec44e5fe27e4eea31689627934ed4717cbfd062
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2022-11-03 20:24:43 -0300

    Indentação

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
-import { Link } from 'react-router-dom';
 
 class Header extends Component {
   state = {
