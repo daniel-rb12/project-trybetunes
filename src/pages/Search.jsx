8bfae78dc8a8fe09e6d270911ebf57535ef882e9
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2022-11-04 15:34:43 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,53 @@ import React, { Component } from 'react';
 import Header from '../Components/Header';
 
 class Search extends Component {
+  state = {
+    search: '',
+    buttonDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      this.activateButton();
+    });
+  };
+
+  activateButton = () => {
+    const { search } = this.state;
+    const minimumCharacters = 2;
+    if (search.length >= minimumCharacters) {
+      this.setState({ buttonDisabled: false });
+    } else {
+      this.setState({ buttonDisabled: true });
+    }
+  };
+
   render() {
+    const { buttonDisabled } = this.state;
+    const { handleChange } = this;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <label htmlFor="search">
+            Pesquisar:
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="search"
+              id="search"
+              onChange={ handleChange }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ buttonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
