b9fd7e476a20624657fead712d8937ec322b1c3d
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2022-11-04 17:46:55 -0300

    Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends Component {
   state = {
     search: '',
     buttonDisabled: true,
+    albums: [],
+    render: false,
+    searched: '',
   };
 
   handleChange = ({ target }) => {
@@ -24,9 +29,17 @@ class Search extends Component {
     }
   };
 
+  searchAlbum = () => {
+    const { search } = this.state;
+    this.setState({ searched: search, search: '' }, async () => {
+      const albumsMatched = await searchAlbumsAPI(search);
+      this.setState({ albums: albumsMatched, render: true });
+    });
+  };
+
   render() {
-    const { buttonDisabled } = this.state;
-    const { handleChange } = this;
+    const { buttonDisabled, search, render, albums, searched } = this.state;
+    const { handleChange, searchAlbum } = this;
     return (
       <div data-testid="page-search">
         <Header />
@@ -38,6 +51,7 @@ class Search extends Component {
               type="text"
               name="search"
               id="search"
+              value={ search }
               onChange={ handleChange }
             />
           </label>
@@ -45,10 +59,29 @@ class Search extends Component {
             data-testid="search-artist-button"
             type="button"
             disabled={ buttonDisabled }
+            onClick={ searchAlbum }
           >
             Pesquisar
           </button>
         </form>
+        <div>
+          { render && <p>{ `Resultado de álbuns de: ${searched}` }</p> }
+          <ul>
+            {albums.map(({ artistName, artworkUrl100, collectionName, collectionId }) => (
+              <li key={ collectionName }>
+                <img src={ artworkUrl100 } alt={ artistName } />
+                <p>{ collectionName }</p>
+                <p>{ artistName }</p>
+                <Link
+                  data-testid={ `link-to-album-${collectionId}` }
+                  to={ `/album/${collectionId}` }
+                >
+                  Escute aqui
+                </Link>
+              </li>))}
+          </ul>
+          { render && albums.length === 0 ? <p>Nenhum álbum foi encontrado</p> : '' }
+        </div>
       </div>
     );
   }
