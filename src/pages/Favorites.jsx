d38b7f16d49747dc27d1f94ebd7a7cafb3b9cd8c
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2022-11-06 12:44:38 -0300

    Requisito 12

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,65 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header';
+import MusicCard from '../Components/MusicCard';
+import Loading from './Loading';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  state = {
+    favorites: [],
+    loading: false,
+    checked: true,
+  };
+
+  componentDidMount() {
+    this.restoreFavorites();
+  }
+
+  handleChange = ({ target }) => {
+    const { name, checked } = target;
+    if (!checked) {
+      this.setState({ loading: true }, async () => {
+        const { favorites } = this.state;
+        const songs = favorites.find((song) => song.trackId === +name);
+        await removeSong(songs);
+        const teste = await getFavoriteSongs();
+        this.setState({ favorites: teste, loading: false });
+      });
+    }
+  };
+
+  restoreFavorites = async () => {
+    this.setState(({ loading: true }), async () => {
+      const box = await getFavoriteSongs();
+      this.setState({ favorites: box, loading: false });
+    });
+  };
+
   render() {
+    const { favorites, loading, checked } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
-        Favorites
+        {
+          loading ? <Loading />
+            : (
+              <div>
+                <ul>
+                  { favorites.map((favorite) => (
+                    <MusicCard
+                      trackName={ favorite.trackName }
+                      previewUrl={ favorite.previewUrl }
+                      trackId={ favorite.trackId }
+                      handleChange={ this.handleChange }
+                      checked={ checked }
+                      key={ favorite.trackId }
+                      loading={ loading }
+                    />
+                  )) }
+                </ul>
+              </div>
+            )
+        }
       </div>
     );
   }
