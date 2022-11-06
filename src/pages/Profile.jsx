20c355e4b97ad4e53c98fd9fd4b6e16506b9369b
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2022-11-06 13:10:25 -0300

    Requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  state = {
+    profile: [],
+  };
+
+  componentDidMount() {
+    this.getProfile();
+  }
+
+  getProfile = async () => {
+    const prof = await getUser();
+    this.setState({ profile: [prof] });
+  };
+
   render() {
+    const { profile } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        {profile.map(({ description, email, image, name }) => (
+          <div key={ name }>
+            <img data-testid="profile-image" src={ image } alt={ name } />
+            <p>{ name }</p>
+            <p>{ email }</p>
+            <p>{ description }</p>
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+        ))}
       </div>
     );
   }
